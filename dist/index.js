1ee4ca02c1334c2ca94766e54b92fba0cb56ce4d
Alexandr <50839940+webobscure@users.noreply.github.com>
2024-02-02 20:54:19 +0300

    Автоматизация добавления продуктов

--- dist/index.js
+++ dist/index.js
@@ -7,31 +7,42 @@ const connection = mysql.createConnection({
   password: "8dYNH846SPvY",
 });
 
-  connection.connect((err) => {
+connection.connect((err) => {
     if (err) throw err;
-    console.log('Connected to the database!');
+    console.log('Успешное подключение к БД!');
     
-    let sql = "SELECT products_specification_id,specification,specifications_id, language_id FROM products_specifications WHERE products_id = 20"
-    connection.query(sql, (err, result) => {
+    let sql = 'SELECT * FROM products_specifications';
+    connection.query(sql, (err, results) => {
       if (err) throw err;
-      //console.log(result)
-      for (let i = 0; i < result.length; i++) {
-        if (result[i].products_specification_id == 753460) {
-            //result[i].specification = (result[i].specification * 2.2).toString();
-            console.log(result[i])
-            
-          let updateSql = `UPDATE products_specifications SET specification = ${result[i].specification} WHERE specifications_id = 23 AND language_id = 2`;
-          /*
+      results.forEach((row)=> {
+        if (row.specifications_id == 767 ) {
+          // чтобы получить единицу в дюймах let newSpecification = (parseFloat(row.specification * 0.04).toFixed(2)).toString();
+           //чтобы получить единицу в фунтах нужно:
+            let newSpecification = (parseFloat(row.specification * 2.2).toFixed(2)).toString();
+          newSpecification = Math.round(newSpecification * 100) / 100;
+        if (newSpecification % 1 === 0) {
+          newSpecification = Math.round(newSpecification);
+        }
+          let productId = row.products_id;
+          row.language_id = 2
+          row.specification = newSpecification
+          console.log(row)
+
+          // запрос на добавление должен выглядеть подобным образом: INSERT INTO products_specifications(products_id,language_id, specification, specifications_id) VALUES('52','2','232.98', '419')
+        //  let updateSql = `UPDATE products_specifications SET specification = ${newSpecification} WHERE products_id = ${productId} AND specifications_id = 23 AND language_id = 2`;
+         
+          let updateSql = `INSERT INTO products_specifications(products_id,language_id, specification, specifications_id) VALUES('${productId}','${row.language_id}','${row.specification}','${row.specifications_id}')`;
           connection.query(updateSql, (err, updateResult) => {
             if (err) throw err;
-            console.log('Updated a record with specification_id equal to 753426:', updateResult);
-            console.log(result[i])
+            console.log('Обновлена таблица у продукта с ID:', productId);
+            console.log(updateResult)
+            console.log(newSpecification)
+            console.log(updateSql)
           });
-          */
-          break;
+          
         }
-      }
-      
+      })
+        
+
     });
   });
-  
